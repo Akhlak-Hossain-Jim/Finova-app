c233d0564aa56c3407c8fca17737db982eff9781
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2025-07-22 02:55:43 +0600

    🛠️📕updated and logs removes

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,24 +1,25 @@
-import { Stack } from 'expo-router';
-import { useRouter } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { useAuth } from '@/contexts/AuthContext';
-import { useEffect } from "react"
+import { useEffect } from 'react';
 
 export default function AuthLayout() {
   const router = useRouter();
-  const { session,loading } = useAuth();
+  const { session, loading } = useAuth();
 
   useEffect(() => {
     if (loading) return;
     if (session) {
-      console.log("user is here")
-    }else {
-      router.replace("/(auth)/signin");
+      return;
+    } else {
+      router.replace('/(auth)/signin');
     }
   }, [session, loading, router]);
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="signin" />
       <Stack.Screen name="signup" />
+      <Stack.Screen name="privacy-policy" />
+      <Stack.Screen name="delete-account" />
     </Stack>
   );
 }
