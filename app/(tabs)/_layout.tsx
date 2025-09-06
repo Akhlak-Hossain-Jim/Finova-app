fb871fd6cf967f3a9ae7d3aab69679dc20f69084
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2025-09-06 17:37:05 +0600

    🛠️added incomes tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -86,6 +86,12 @@ export default function TabLayout() {
           href: null,
         }}
       />
+      <Tabs.Screen
+        name="incomes"
+        options={{
+          href: null,
+        }}
+      />
     </Tabs>
   );
 }
