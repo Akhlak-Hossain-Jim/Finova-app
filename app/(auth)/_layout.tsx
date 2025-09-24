d4c596104eac86a97f4675cd8ab9dddc1572673f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2025-09-25 03:14:00 +0600

    🛠️tab switch glitch fix

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,16 +1,6 @@
-import { useAuth } from '@/contexts/AuthContext';
-import { router, Stack } from 'expo-router';
-import { useEffect } from 'react';
+import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
-  const { session, loading } = useAuth();
-
-  useEffect(() => {
-    if (loading) return;
-    if (session) return;
-    router.replace('/(auth)/welcome');
-  }, [session, loading]);
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="welcome" />
